700ebfac1cc00fbe5effe3ef393b987ed2910c41
Piotr Wesolowski <piowes@uxs-macbook-pro.corp.capgemini.com>
2017-11-14 14:43:36 +0100

    Final app version after refactorisation.

---
+++ CarWash/Helper/ControllerViewSettings.swift
@@ -0,0 +1,9 @@
+//
+//  ControllerViewSettings.swift
+//  CarWash
+//
+//  Created by Piotr Wesolowski on 14/11/2017.
+//  Copyright © 2017 Piotr Wesolowski. All rights reserved.
+//
+
+import Foundation
